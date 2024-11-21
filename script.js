dc17cf4a9c437eefd33ecd933568a50ae54caa77
Mayuri Umaretiya <mayuriumaretiya111@gmail.com>
2024-11-21 16:45:50 +0530

    Dom in javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+// Console code for javascript
+
+document.getElementById('heading')
+
+document.getElementById('title').id
+
+document.getElementById('heading').className
+
+document.getElementById('heading').getAttribute('id')
